03ac6d059dfdb2c13e5ff98dbd98f74afdb6421d
Bekir95 <bekir.aydn42@gmail.com>
2025-04-06 07:30:44 +0300

    first commit

--- main/models.py
+++ main/models.py
@@ -8,3 +8,5 @@ class Activities(models.Model):
     Baslik = models.CharField(max_length=200 , null=True , blank=True)
     Aciklama = models.TextField(null=True , blank=True)
     Resim = models.ImageField(upload_to='djangouploads/mainActivities', null=True , blank=True)
+
+
